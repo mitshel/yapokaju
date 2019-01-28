0db30bb26e48070864d3dcc29b5c8ba4d7efce7b
Dmitriy Sergeev <dmitriy.93.sergeev@gmail.com>
2019-01-28 17:56:55 +0300

    1. Обновил конфигурации.

--- conf/settings.py
+++ conf/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'g6qtgbcvn=yuep2_&(5_sj8wi#3^e@y+-&6)c$t-ecq(%*_%f0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pyhammer.beget.tech']
+ALLOWED_HOSTS = ['127.0.0.1', '.pyhammer.beget.tech', '.xn--80amkpe2a2f.xn--p1ai']
 
 INTERNAL_IPS = ['127.0.0.1', ]
 
@@ -153,21 +153,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-DJANGO_STATIC = True
-
 STATIC_URL = '/static/'
 
-if not DJANGO_STATIC:
-    STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
-
-if DJANGO_STATIC:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
-    ]
 
 
 # https://docs.djangoproject.com/en/2.1/topics/email/#smtp-backend
