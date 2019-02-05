77aaf22ae0843d99e75022f4120b8c01e3a5ed43
Dmitriy Sergeev <dmitriy.93.sergeev@gmail.com>
2019-02-05 21:54:11 +0300

    1. Отправка уведомления организатору мероприятия.

--- apps/clndr/apps.py
+++ apps/clndr/apps.py
@@ -5,3 +5,6 @@ class ClndrConfig(AppConfig):
     name = 'apps.clndr'
 
     verbose_name = 'The calendar'
+
+    def ready(self):
+        from . import signals
---
+++ apps/clndr/signals.py
@@ -0,0 +1,27 @@
+from django.conf import settings
+from django.core.mail import EmailMessage, send_mail
+from django.db.models.signals import post_save
+from django.dispatch import Signal, receiver
+from django.utils import timezone
+
+from apps.core.utils import format_phone
+
+from .models import Member
+
+
+@receiver(post_save, sender=Member)
+def send_mail_to_organizer(sender, instance, created, **kwargs):
+    if created:
+        subject = 'Подписка на мероприятие'
+        text = ' '.join([
+            'На Ваше мероприятие',
+            '"' + instance.event.template.name + '"',
+            'запланированное время:',
+            timezone.localtime(instance.datetime.datetime).strftime('%d.%m.%Y г. в %H:%M (МСК)'),
+            'подписалися(лась)',
+            instance.user.get_full_name(),
+            instance.user.email,
+            format_phone(instance.user.phone)
+        ])
+        email = EmailMessage(subject, text, settings.DEFAULT_FROM_EMAIL, [instance.event.user.email, ])
+        email.send()
