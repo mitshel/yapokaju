b5cc557a609217800c33d88345138bcbbfd36994
Dmitry Shelepnev <mitshel@mail.ru>
2019-02-06 00:00:35 +0300

    change mail text

--- apps/clndr/signals.py
+++ apps/clndr/signals.py
@@ -14,14 +14,16 @@ def send_mail_to_organizer(sender, instance, created, **kwargs):
     if created:
         subject = 'Подписка на мероприятие'
         text = ' '.join([
+            'Уважаемый ВОЛОНТЕР.'
             'На Ваше мероприятие',
             '"' + instance.event.template.name + '"',
             'запланированное время:',
             timezone.localtime(instance.datetime.datetime).strftime('%d.%m.%Y г. в %H:%M (МСК)'),
-            'подписалися(лась)',
+            'подписался(лась)',
             instance.user.get_full_name(),
             instance.user.email,
-            format_phone(instance.user.phone)
+            format_phone(instance.user.phone),'\n',
+            'Пожалуйста свяжитесь с человеком, которому требуется Ваша помощь по указанному телефону, для обсуждения деталей экскурсии'
         ])
         email = EmailMessage(subject, text, settings.DEFAULT_FROM_EMAIL, [instance.event.user.email, ])
         email.send()
