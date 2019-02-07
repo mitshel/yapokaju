c408a9f5ba988ee69f6ebcacec4661e9ae620a61
Dmitry Shelepnev <mitshel@mail.ru>
2019-02-07 15:20:00 +0300

    Add Subscription delete mail

--- apps/clndr/signals.py
+++ apps/clndr/signals.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.core.mail import EmailMessage, send_mail
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch import Signal, receiver
 from django.utils import timezone
 
@@ -17,7 +17,8 @@ def send_mail_to_organizer(sender, instance, created, **kwargs):
             'Уважаемый ВОЛОНТЕР.\nНа Ваше мероприятие',
             '"' + instance.event.template.name + '"',
             ', дата:',
-            timezone.localtime(instance.datetime.datetime).strftime('%d.%m.%Y'),
+            #timezone.localtime(instance.datetime.datetime).strftime('%d.%m.%Y'),
+            instance.datetime.datetime.strftime('%d.%m.%Y'),
             ', подписался(лась)',
             instance.user.get_full_name()+'.',
             '\nE-mail:',
@@ -29,3 +30,22 @@ def send_mail_to_organizer(sender, instance, created, **kwargs):
         ])
         email = EmailMessage(subject, text, settings.DEFAULT_FROM_EMAIL, [instance.event.user.email, ])
         email.send()
+
+@receiver(pre_delete, sender=Member)
+def send_mail_to_organizer_delte(sender, instance, **kwargs):
+    subject = 'Отмена подписка на мероприятие'
+    text = ' '.join([
+        'Уважаемый ВОЛОНТЕР.\nНа Ваше мероприятие',
+        '"' + instance.event.template.name + '"',
+        ', дата:',
+        #timezone.localtime(instance.datetime.datetime).strftime('%d.%m.%Y'),
+        instance.datetime.datetime.strftime('%d.%m.%Y'),
+        ', была отменена подписка пользователем',
+        instance.user.get_full_name()+'.',
+        '\nE-mail:',
+        instance.user.email,
+        '\nТелефон:',
+        format_phone(instance.user.phone)
+    ])
+    email = EmailMessage(subject, text, settings.DEFAULT_FROM_EMAIL, [instance.event.user.email, ])
+    email.send()
